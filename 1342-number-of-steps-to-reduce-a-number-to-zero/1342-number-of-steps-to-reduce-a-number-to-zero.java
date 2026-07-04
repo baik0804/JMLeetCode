deece6538c0499c0bd45b76349fe569d4957b66e
baik0804 <102820266+baik0804@users.noreply.github.com>
2026-07-05 00:50:18 +0900

    Time: 0 ms (100%), Space: 42.3 MB (37.13%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -3,13 +3,12 @@ class Solution {
         int steps = 0;
         while(num>0){
             if(num%2==0){
-                steps++;
                 num /= 2;
             }
             else{
-                steps++;
                 num -= 1;
             }
+            steps++;
         }
         return steps;
     }
