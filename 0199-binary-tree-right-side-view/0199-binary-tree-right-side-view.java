737125f4a0507c0243f521f6572a1a2ce9bb49bb
baik0804 <102820266+baik0804@users.noreply.github.com>
2026-08-17 20:48:27 +0900

    Time: 0 ms (100%), Space: 43.6 MB (51.43%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,44 +1,18 @@
-import java.util.LinkedList;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-
-
-//Definition for a binary tree node.
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
+        List<Integer> view = new ArrayList<>();
         if(root == null)
-            return res;
+            return view;
+        dfs(root,0,view);
+        return view;
+    }
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int i=0;i<size;i++){
-                TreeNode currentNode = queue.poll();
-                if(currentNode.left != null)
-                    queue.offer(currentNode.left);
-                if(currentNode.right != null)
-                    queue.offer(currentNode.right);
-                if(i == size - 1)
-                    res.add(currentNode.val);
-            }
-        }
-        return res;
+    public void dfs(TreeNode root, int level, List<Integer> view) {
+        if(root == null)
+            return;
+        if(level == view.size())
+            view.add(root.val);
+        dfs(root.right,level+1,view);
+        dfs(root.left,level+1,view);
     }
 }
