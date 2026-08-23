16547d43cf6391ab816ad894d422439aaff94e0c
baik0804 <102820266+baik0804@users.noreply.github.com>
2026-08-23 20:30:25 +0900

    Time: 9 ms (95.27%), Space: 46.1 MB (18.06%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,18 +1,32 @@
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 class Solution {
     public int numSquares(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        
-        dp[0] = 0;
-        
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j*j <= i;j++) {
-                dp[i] = Math.min(dp[i], dp[i-j*j] + 1);
+        Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[n+1];
+        int level = 0;
+
+        visited[n] = true;
+        queue.add(n);
+
+        while(!queue.isEmpty()){
+            level++;
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                int cur = queue.poll();
+                for(int j = 1; j*j <= cur; j++){
+                    int next = cur - j*j;
+                    if(next == 0){
+                        return level;
+                    }
+                    if(!visited[next]){
+                        visited[next] = true;
+                        queue.add(next);
+                    }
+                }
             }
         }
-        
-        return dp[n];
+        return level;
     }
 }
